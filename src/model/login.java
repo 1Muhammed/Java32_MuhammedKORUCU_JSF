d6dfe877581c79d0fb0aa1b5012f5b335f1fdcd9
vektorel <vektorel@lab11-6>
2018-06-23 14:42:50 +0300

    Projem

--- src/model/login.java
+++ src/model/login.java
@@ -11,8 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/login")
 public class login extends HttpServlet {
 	
-	private static final long serialVersionUID = 1L;
-   
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String email = request.getParameter("email");
